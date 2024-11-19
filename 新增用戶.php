215bd6bd1e9f9e1faa1164448b412f47fe3e3fe2
109534237 <109534237@stu.ukn.edu.tw>
2024-11-19 17:11:10 +0800

    4

--- 新增用戶.php
+++ 新增用戶.php
@@ -34,7 +34,7 @@ if (isset($_SESSION["帳號"]) && isset($_SESSION["姓名"])) {
 
 <head>
     <meta charset="utf-8">
-    <title>健康醫療網站</title>
+    <title>健康醫療網站 </title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Free HTML Templates" name="keywords">
     <meta content="Free HTML Templates" name="description">
